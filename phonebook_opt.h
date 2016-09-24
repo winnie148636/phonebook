e99529a24440f443dc4aa1cdbc5d6fe4b580de1d
winnie148636 <winnie148636@gmail.com>
2016-09-24 16:50:09 +0800

    minimize the size of phonebook entry

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,9 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+typedef struct _PHONE_DETAIL {
+
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +17,24 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} detail_entry;
+
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    /*char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5]; */
+    detail_entry *pDetail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
